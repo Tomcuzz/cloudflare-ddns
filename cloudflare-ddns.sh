5754a1b08eb940c5deb33941b34fc4d98c5c4bc3
Thomas Cousin <thomas.cousin@me.com>
2020-08-22 20:39:44 +0100

    Updates to ip aquisition process due to ipconf.co requiring recapture

--- cloudflare-ddns.sh
+++ cloudflare-ddns.sh
@@ -3,4 +3,4 @@
 curl -X PUT "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$RECORD_ID" \
      -H "Authorization: Bearer $AUTH_KEY" \
      -H "Content-Type: application/json" \
-     --data "{\"type\":\"A\",\"name\":\"$NAME\",\"content\":\"`curl ifconfig.co`\"}"
+     --data "{\"type\":\"A\",\"name\":\"$NAME\",\"content\":\"`dig +short myip.opendns.com @resolver1.opendns.com`\"}"
